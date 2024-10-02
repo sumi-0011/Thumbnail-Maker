b4c034b7b52cef2c108ef4c4a7b0ee17dd4d0fce
sumi-0011 <selina2000@naver.com>
2024-10-02 18:59:10 +0900

    feat: thumnail maker template 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,15 @@
-import { Helmet } from 'react-helmet'
-import { useTranslation } from 'react-i18next'
-import { Hero } from 'src/components/hero'
+import { Helmet } from "react-helmet";
+import { useTranslation } from "react-i18next";
+import ThumbnailMaker from "src/components/thumbnail-maker";
 
 export default function Home() {
-  const { t } = useTranslation('translation')
+  const { t } = useTranslation("translation");
   return (
     <>
       <Helmet>
-        <title>{t('title')}</title>
+        <title>Thumbnail Maker</title>
       </Helmet>
-      <Hero />
+      <ThumbnailMaker />
     </>
-  )
+  );
 }
