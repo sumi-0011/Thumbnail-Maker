404636dae99aae337d80cecd723cdfbfe929c836
sumi-0011 <selina2000@naver.com>
2024-10-04 17:17:02 +0900

    feat: tag 제거 로직 정상화

--- src/components/thumbnail-maker/TagItem.tsx
+++ src/components/thumbnail-maker/TagItem.tsx
@@ -11,24 +11,26 @@ export function TagItem({
   onRemove: () => void;
 }) {
   return (
-    <div
-      style={{
-        ...tagSize.base,
-        ...tagStyle[`${tag.tagVariant}-${tag.tagShape}`],
-      }}
-      className={cn(
-        "relative flex min-w-fit max-w-full cursor-pointer items-center overflow-hidden truncate whitespace-nowrap rounded-full transition-all duration-300 ease-in-out"
-      )}
-    >
-      <span>{tag.text}</span>
+    <div className="group relative">
       <div
-        className="absolute -right-1 -top-1 hidden cursor-pointer rounded-full bg-white/80 p-0.5"
+        style={{
+          ...tagSize.base,
+          ...tagStyle[`${tag.tagVariant}-${tag.tagShape}`],
+        }}
+        className={cn(
+          "relative flex min-w-fit max-w-full cursor-pointer items-center overflow-hidden truncate whitespace-nowrap rounded-full transition-all duration-300 ease-in-out"
+        )}
+      >
+        <span>{tag.text}</span>
+      </div>
+      <div
+        className="absolute -right-1 -top-1  hidden h-6 w-6 cursor-pointer justify-center rounded-full bg-white/80 p-0.5 align-middle group-hover:flex"
         onClick={(e) => {
           e.stopPropagation();
           onRemove();
         }}
       >
-        <X size={12} color="#ff4d4f" />
+        <X size={20} color="#454545" />
       </div>
     </div>
   );
