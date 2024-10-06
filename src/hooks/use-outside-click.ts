dec4b2c5f60f26c73d8c060dea5a0e20a9bcd85b
sumi-0011 <selina2000@naver.com>
2024-10-06 21:23:16 +0900

    feat: emoji picker 외부를 클릭하면 닫힐수 있도록

---
+++ src/hooks/use-outside-click.ts
@@ -0,0 +1,20 @@
+import { useEffect, RefObject } from "react";
+
+export const useOutsideClick = <T extends HTMLElement = HTMLElement>(
+  ref: RefObject<T>,
+  handler: (event: MouseEvent) => void
+) => {
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (!ref.current || ref.current.contains(event.target as Node)) {
+        return;
+      }
+      handler(event);
+    };
+
+    document.addEventListener("mousedown", listener);
+    return () => {
+      document.removeEventListener("mousedown", listener);
+    };
+  }, [ref, handler]);
+};
