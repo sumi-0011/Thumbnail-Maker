485457614f12297c54e67fe5011adf739aaed124
sumi-0011 <selina2000@naver.com>
2024-10-06 21:23:16 +0900

    feat: emoji picker 외부를 클릭하면 닫힐수 있도록

--- src/components/thumbnail-maker/EmojiPicker.tsx
+++ src/components/thumbnail-maker/EmojiPicker.tsx
@@ -1,10 +1,15 @@
 import { Picker, EmojiType } from "ms-3d-emoji-picker";
 import { Button } from "../ui/button";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { SmilePlusIcon } from "lucide-react";
+import { useOutsideClick } from "src/hooks/use-outside-click";
 
 function EmojiPicker({ onAction }: { onAction: (emoji: EmojiType) => void }) {
   const [isOpen, setIsOpen] = useState(false);
+
+  const pickerRef = useRef(null);
+  useOutsideClick(pickerRef, () => setIsOpen(false));
+
   return (
     <div className="relative">
       <Button
@@ -15,7 +20,7 @@ function EmojiPicker({ onAction }: { onAction: (emoji: EmojiType) => void }) {
       >
         <SmilePlusIcon size={18} color="#6A83FF" />
       </Button>
-      <div className="absolute right-0 mt-2">
+      <div className="absolute right-0 z-10 mt-2" ref={pickerRef}>
         <Picker
           isOpen={isOpen}
           handleEmojiSelect={(selectedEmoji: EmojiType) =>
