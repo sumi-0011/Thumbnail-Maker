027f8ad03028bc8b47e4bb6bb0ccc148b7b78c12
sumi-0011 <selina2000@naver.com>
2024-10-10 00:22:00 +0900

    feat: error page 추가

--- src/components/error-page/index.tsx
+++ src/components/error-page/index.tsx
@@ -1,22 +1,32 @@
-import { useTranslation } from 'react-i18next'
-import { Link, useRouteError } from 'react-router-dom'
-import { Button } from '../ui/button'
+import { Link } from "react-router-dom";
+import { Button } from "../ui/button";
 
 export default function ErrorPage() {
-  const error = useRouteError() as any
-  const { t } = useTranslation('notfound')
-
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center gap-4 text-left">
-      <h1>{t('oops')}</h1>
-      <p>{t('title')}</p>
-      <p className="font-mono">
-        <span className="mr-2">{error?.status}</span>
-        <i>{error?.statusText || error?.message}</i>
-      </p>
-      <Button asChild>
-        <Link to="/">{t('backtohomepage')}</Link>
-      </Button>
+    <div className="flex h-screen w-full flex-col items-center justify-center text-white">
+      <div className="rounded-lg bg-white bg-opacity-10 p-8 text-center backdrop-blur-md">
+        {/* <FailIcon className="mx-auto mb-4 h-24 w-24 animate-bounce text-yellow-300" /> */}
+        <div className="flex items-center justify-center">
+          <img
+            src="https://avahrjwyynzeocqpyfhw.supabase.co/storage/v1/object/public/3d-fluent-emojis//smileys&emotion/crying_face/3D/crying_face_3d.png"
+            alt="sorry"
+            className="h-24 w-24"
+          />
+        </div>
+        <h1 className="mb-4 mt-4 text-4xl font-bold">
+          Oops, something went wrong!
+        </h1>
+        <p className="mb-6 text-xl">Sorry, an unexpected error has occurred.</p>
+        <p className="mb-8 text-lg">Don't worry, we're on it!</p>
+        <Button
+          asChild
+          className="bg-white text-purple-600 hover:bg-purple-100"
+        >
+          <Link to="/" className="inline-flex items-center">
+            <span className="mr-2">🏠</span> Return to Home
+          </Link>
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
