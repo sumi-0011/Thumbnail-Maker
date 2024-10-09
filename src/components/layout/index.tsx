cf577db5d6137c93d20a0a99fc8563113056c630
sumi-0011 <selina2000@naver.com>
2024-10-09 19:05:01 +0900

    feat: layout fix

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -5,7 +5,7 @@ export const getNoneLayout = (page: React.ReactElement) => page;
 
 export const getDefaultLayout = (page: React.ReactElement) => {
   return (
-    <div className="h-min-screen flex min-h-screen items-center justify-center bg-[#1D2027]">
+    <div className="h-min-screen flex h-[900px] min-h-screen w-fit items-center justify-center bg-[#1D2027] p-6">
       {/* <Header /> */}
       {page}
     </div>
