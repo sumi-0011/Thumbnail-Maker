db136a554004e91eb03bc7a596672a9348f85e78
sumi-0011 <selina2000@naver.com>
2024-10-09 23:43:45 +0900

    feat: ga 심기

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,18 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router-dom";
 import { createRouter } from "./router";
 import { ThemeProvider } from "./components/theme-provider";
 import { Toaster } from "./components/ui/sonner";
+import ReactGA from "react-ga4";
 
 export default function App() {
   const queryClient = useMemo(() => new QueryClient({}), []);
+
+  useEffect(() => {
+    ReactGA.initialize(`${import.meta.env.VITE_APP_GA_ID}`);
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
