187fa621972da1ad9f838ab9366efa02fd542547
sumi-0011 <selina2000@naver.com>
2024-10-11 02:28:35 +0900

    feat: template sub menu button

--- src/components/thumbnail-maker/TemplateSaveButton.tsx
+++ src/components/thumbnail-maker/TemplateSaveButton.tsx
@@ -1,15 +1,40 @@
-import { ScrollText } from "lucide-react";
+import { CircleEllipsis, Menu } from "lucide-react";
 import { Button } from "../ui/button";
-import { useTagList } from "./Tag.context";
+import { useTagAction, useTagList } from "./Tag.context";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarTrigger,
+} from "../ui/menubar";
+import { Link } from "react-router-dom";
 
 export function TemplateSaveButton() {
   const { tags } = useTagList();
+  const { onResetTags } = useTagAction();
 
   return (
     <div>
-      <Button variant="secondary">
-        <ScrollText size={20} />
-      </Button>
+      <Menubar className="bg-transparent">
+        <MenubarMenu>
+          <MenubarTrigger asChild>
+            <Button variant="secondary">
+              <Menu />
+            </Button>
+          </MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem onClick={onResetTags}>Reset Canvas</MenubarItem>
+            <MenubarSeparator />
+            <Link to="/gallery">
+              <MenubarItem>Go to Gallery</MenubarItem>
+            </Link>
+            <MenubarItem disabled>Save Template (preparing)</MenubarItem>
+            <MenubarSeparator />
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
     </div>
   );
 }
